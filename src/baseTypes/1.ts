e3c81043a55589c7758f542ec6a16b1fab31b836
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-10-28 22:52:48 +0300

    Update 1.ts

--- src/baseTypes/1.ts
+++ src/baseTypes/1.ts
@@ -3,8 +3,8 @@
 let age:number = 50;
 let name:string = 'Max';
 let toggle:boolean = true;
-let empty = null;
-let notInitialize;
-let callback = (a) => { return 100 + a };
+let empty:null = null;
+let notInitialize:any;
+let callback = (a:number) => { return 100 + a };
 
 export {};
