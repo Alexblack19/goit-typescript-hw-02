8d93da0e29353a67b0304f192025d8c6d7af1ceb
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-10-29 20:12:03 +0200

    update

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -3,6 +3,23 @@
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
 
-function isWeekend (): boolean {
-  return true
+enum daysOfWeek {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
 }
+
+function isWeekend(day: daysOfWeek): boolean {
+  return day === daysOfWeek.Saturday || day === daysOfWeek.Sunday
+    ? false
+    : true;
+}
+
+console.log("It's a day off?", isWeekend(daysOfWeek.Saturday)); // false
+console.log("It's a day off?", isWeekend(daysOfWeek.Sunday)); // false
+console.log("It's a day off?", isWeekend(daysOfWeek.Thursday)); // true
+console.log("It's a day off?", isWeekend(daysOfWeek.Friday)); // true
